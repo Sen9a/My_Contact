a3e1424c563bbe95c82ec9931a8bf4c4873eebab
Octo <sen9a1990@gmail.com>
2017-06-28 09:25:26 +0300

    Rename fixture

--- hello/migrations/0002_fixture_info.py
+++ hello/migrations/0002_fixture_info.py
@@ -8,11 +8,8 @@ class Migration(DataMigration):
 
     def forwards(self, orm):
         "Write your forwards methods here."
-        # Note: Don't use "from appname.models import ModelName". 
-        # Use orm.ModelName to refer to models in this application,
-        # and orm['appname.ModelName'] for models in other applications.
         from django.core.management import call_command
-        call_command("loaddata", "initial_data.json")
+        call_command("loaddata", "my_info.json")
 
     def backwards(self, orm):
         "Write your backwards methods here."
